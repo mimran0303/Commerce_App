d7e389fd6c10e9a67605a77479ea7abb4f501d59
fatim <mfimran0303@gmail.com>
2025-11-30 19:37:12 -0600

    Add project files.

---
+++ OrderTracker/Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderTracker.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public int Price { get; set; }
+    }
+}
