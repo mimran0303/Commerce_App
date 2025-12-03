697ca09dcb1b73140f1eb92c26ad190caceef138
fatim <mfimran0303@gmail.com>
2025-12-02 22:04:51 -0600

    added new class Product_Prop

---
+++ OrderTracker/Model/Product_Prop.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace OrderTracker.Model
+{
+    public class Product_Prop
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public double Price { get; set; }
+        public string Value { get; set; }
+        public IEnumerable<Product_Prop> ProductProperties { get; set; }
+
+    }
+}
