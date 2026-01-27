7acc48aaedff6451feff2aeabc93b97b687769f4
fatim <mfimran0303@gmail.com>
2026-01-27 14:08:57 -0600

    Toastr Notifications working

---
+++ OrderTracker/wwwroot/js/common.js
@@ -0,0 +1,11 @@
+﻿window.showToastr = function (type, message) {
+
+    if (type == "success") {
+        toastr.success(message);
+    }
+
+    if (type == "error") {
+        toastr.error(message);
+    }
+
+}
